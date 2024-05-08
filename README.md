04ec098ec802a7981b62827c5e6f018fb694c1f0
RiverGang <sory304@naver.com>
2024-05-08 17:24:42 +0900

    6일차 완료

--- README.md
+++ README.md
@@ -113,12 +113,15 @@ IoT 개발자 과정 WPF 학습리포지토리
 
 - Movie API 연동 앱, MovieFinder 2024
     - DB(SQL Server) 연동
-    - MahApps.Metro
+    - MahApps.Metro UI
+    - CefSharp WebBrowser 패키지
+    - Google.Apis 패키지
     - OpenAPI 두가지 사용
-    - MVVM은 시간부족으로 사용x
+    - MVVM 사용x
 
     - 좋아하는 영화 즐겨찾기 앱
     - [TMDB](https://www.themoviedb.org/) OpenAPI 활용
+        - 회원가입 후 API 신청
     - [Youtube API](https://console.cloud.google.com/) 활용
         - 새 프로젝트 생성
         - API 및 서비스, 라이브러리 선택
@@ -128,3 +131,17 @@ IoT 개발자 과정 WPF 학습리포지토리
             2. OAutho 동의화면, 기본내용 입력 후 다음
             3. 범위는 저장 후 계속
             4. OAuth Client ID, 앱유형 -> 데스크톱앱, 이름 입력 후 만들기 클릭
+
+## 6일차
+- MovieFinder 2024 남은 것
+    - 즐겨찾기 후 다시 선택 즐겨찾기 막기
+    - 즐겨찾기 삭제 구현
+    - 그리드뷰 영화 더블클릭 시, 영화소개 팝업
+
+
+## 7일차
+- 데이터포털 API 연동앱 예제
+    - 7일차 개인프로젝트 참조 소스
+
+## 8일차
+- WPF 개인프로젝트 포트폴리오 작업
--- day05/wpf05_mvvm_app/ex10_MovieFinder2024/MainWindow.xaml.cs
+++ day05/wpf05_mvvm_app/ex10_MovieFinder2024/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -12,8 +13,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ex10_MovieFinder2024.models;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using Microsoft.Win32.SafeHandles;
+using Newtonsoft.Json.Linq;
 
 namespace ex10_MovieFinder2024
 {
@@ -22,9 +28,14 @@ namespace ex10_MovieFinder2024
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private bool isFavorite = false; // 즐겨찾기 or API 검색 구분: false => openAPI, true=> 즐겨찾기보기
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            TxtMovieName.Focus();
         }
 
         private async void BtnSearch_Click(object sender, RoutedEventArgs e)
@@ -76,29 +87,179 @@ namespace ex10_MovieFinder2024
                 reader.Close();
                 res.Close();
             }
+
+            // result의 string을 json으로 변경
+            // json: 딕셔너리 형태와 동일. Key와 Value가 한쌍을 이루는 형태
+            var jsonResult = JObject.Parse(result); // type.Parse(string) -> string을 type의 자료형으로 변경
+            var total = Int32.Parse(jsonResult["total_results"].ToString()); // total_results 라는 Key의 Value가 숫자이니 int형태로 변환(Convert)
+            //await this.ShowMessageAsync("검색수", total.ToString()); // int형의 total을 string으로 변환
+            var results = jsonResult["results"];
+            var jsonArray = results as JArray; // results를 Json 배열 형태로 변환 -> foreach 사용가능
+
+
+            var movieItems = new List<MovieItem>(); // MovieItem(models 폴더 내 클래스) Type의 List 선언
+            foreach (var item in jsonArray)
+            {
+                var movieItem = new MovieItem() // 각 요소들 대입
+                {
+                    Adult = Boolean.Parse(item["adult"].ToString()),
+                    Id = Int32.Parse(item["id"].ToString()),
+                    Original_Language = item["adult"].ToString(),
+                    Original_Title = item["original_title"].ToString(),
+                    Overview = item["overview"].ToString(),
+                    Popularity = Double.Parse(item["popularity"].ToString()),
+                    Poster_Path = item["poster_path"].ToString(),
+                    Release_Date = item["release_date"].ToString(),
+                    Title = item["title"].ToString(),
+                    Vote_Average = Double.Parse(item["vote_average"].ToString()),
+                    Vote_Count = Int32.Parse(item["vote_count"].ToString())
+                };
+                movieItems.Add(movieItem); // 위의 리스트에 요소 추가
+            }
+
+            this.DataContext = movieItems;
         }
 
         private void TxtMovieName_KeyDown(object sender, KeyEventArgs e)
         {
-
+            if (e.Key == Key.Enter)
+            {
+               BtnSearch_Click(sender, e);
+            }
         }
 
         private async void GrdResult_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
-            await this.ShowMessageAsync("포스터", "포스터 처리");
+            // 재검색하면 데이터그리 결과가 바뀌면서 이 이벤트가 다시 발생
+            try
+            {
+                var movie = GrdResult.SelectedItem as MovieItem;
+                var poster_path = movie.Poster_Path;
+                if(string.IsNullOrEmpty(poster_path)) 
+                {
+                    ImgPoster.Source = new BitmapImage(new Uri("sources/No_Picture.png", UriKind.RelativeOrAbsolute)); 
+                }
+                else
+                {
+                    var base_url = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
+                    ImgPoster.Source = new BitmapImage(new Uri($"{base_url}{poster_path}", UriKind.Absolute));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"{ex.Message}");
+            }
+            
+
+            //await this.ShowMessageAsync("포스터", poster_path);
 
         }
 
         private async void BtnAddFavorite_Click(object sender, RoutedEventArgs e)
         {
-            await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 추가");
+            //await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 추가");
+            if(GrdResult.SelectedItems.Count == 0) 
+            {
+                await this.ShowMessageAsync("즐겨찾기", "추가할 영화를 선택하세요 (복수선택 가능)");
+                return;
+            }
+            
+            var addMovieItems = new List<MovieItem>();
+            foreach (MovieItem item in GrdResult.SelectedItems)
+            {
+                addMovieItems.Add(item);
+            }
+
+            Debug.WriteLine(addMovieItems.Count);
+
+            try
+            {
+                var insRes = 0;
+                
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+
+                    foreach (var item in addMovieItems)
+                    {
+                        SqlCommand cmd = new SqlCommand(models.MovieItem.INSERT_QUERY, conn);
+                        
+                        cmd.Parameters.AddWithValue("@Id", item.Id);
+                        cmd.Parameters.AddWithValue("@Title", item.Title);
+                        cmd.Parameters.AddWithValue("@Original_Title", item.Original_Title);
+                        cmd.Parameters.AddWithValue("@Release_Date", item.Release_Date);
+                        cmd.Parameters.AddWithValue("@Original_Language", item.Original_Language);
+                        cmd.Parameters.AddWithValue("@Adult", item.Adult);
+                        cmd.Parameters.AddWithValue("@Popularity", item.Popularity);
+                        cmd.Parameters.AddWithValue("@Vote_Average", item.Vote_Average);
+                        cmd.Parameters.AddWithValue("@Vote_Count", item.Vote_Count);
+                        cmd.Parameters.AddWithValue("@Poster_Path", item.Poster_Path);
+                        cmd.Parameters.AddWithValue("@Overview", item.Overview);
+
+                        insRes += cmd.ExecuteNonQuery(); // 데이터 하나마다 INSERT쿼리 실행
+                    }
+
+                    if (insRes == addMovieItems.Count) { await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 저장성공"); }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await this.ShowMessageAsync("오류", $"즐겨찾기 오류 {ex.Message}");
+            }
 
         }
 
         private async void BtnViewFavorite_Click(object sender, RoutedEventArgs e)
         {
-            await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 보기");
+            this.DataContext = null; // 데이터그리드에 보낸 데이터를 모두 삭제
+            TxtMovieName.Text = string.Empty;
 
+
+            List<MovieItem> favMovieItems = new List<MovieItem>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+                    var cmd = new SqlCommand(models.MovieItem.SELECT_QUERY, conn);
+                    var adapter = new SqlDataAdapter(cmd);
+                    var dSet = new DataSet();
+                    adapter.Fill(dSet, "MovieItem");
+
+                    foreach (DataRow row in dSet.Tables["MovieItem"].Rows)
+                    {
+                        var movieItem = new MovieItem()
+                        {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Title = Convert.ToString(row["Title"]),
+                            Original_Title = Convert.ToString(row["Original_title"]),
+                            Release_Date = Convert.ToString(row["Release_Date"]),
+                            Original_Language = Convert.ToString(row["Original_Language"]),
+                            Adult = Convert.ToBoolean(row["Adult"]),
+                            Vote_Average = Convert.ToDouble(row["Vote_Average"]),
+                            Vote_Count = Convert.ToInt32(row["Vote_Count"]),
+                            Poster_Path = Convert.ToString(row["Poster_Path"]),
+                            Overview = Convert.ToString(row["Overview"]),
+                            Reg_Date = Convert.ToDateTime(row["Reg_Date"])
+                        };
+                        favMovieItems.Add(movieItem);
+                    }
+
+                    this.DataContext = favMovieItems;
+                    isFavorite = true; // 즐겨찾기
+                    StsResult.Content = $"{favMovieItems.Count}건 조회완료";
+                    ImgPoster.Source = new BitmapImage(new Uri("sources/No_Picture.png", UriKind.RelativeOrAbsolute));
+                }
+            }
+            catch (Exception ex)
+            {
+                await this.ShowMessageAsync("오류", $"즐겨찾기 조회오류 {ex.Message}");
+            }
         }
 
         private async void BtnDelFavorite_Click(object sender, RoutedEventArgs e)
@@ -109,8 +270,23 @@ namespace ex10_MovieFinder2024
 
         private async void BtnWatchTrailer_Click(object sender, RoutedEventArgs e)
         {
-            await this.ShowMessageAsync("유튜브 예고편", "예고편 확인");
+            if (GrdResult.SelectedItems.Count == 0)
+            {
+                await this.ShowMessageAsync("Youtube Trailer", "영화를 선택하세요");
+                return;
+            }
 
+            if(GrdResult.SelectedItems.Count > 1)
+            {
+                await this.ShowMessageAsync("Youtube Trailer", "영화를 하나만 선택하세요");
+                return;
+            }
+
+            var movieName = (GrdResult.SelectedItem as MovieItem).Title; // 부모창에서 자식창으로 정보 넘기기
+            var trailerWindow = new TrailerWindow(movieName);
+            trailerWindow.Owner = this; // trailerWindow창의 부모는 현재(MainWindow)임을 지정
+            trailerWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner; // 부모창을 기준으로 Center
+            trailerWindow.ShowDialog(); // 창 띄우기
         }
 
     }
